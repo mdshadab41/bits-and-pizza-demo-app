936c849e4e87d985be50b49491f7f46505f838ee
Shadab <shadabjhu00447@gmail.com>
2023-09-23 16:09:15 +0530

    commit 1 -  Diffutil and Data Binding: Life in the Fast Lane

--- app/src/main/java/com/example/tasks_app/TasksFragment.kt
+++ app/src/main/java/com/example/tasks_app/TasksFragment.kt
@@ -38,7 +38,7 @@ class TasksFragment : Fragment() {
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
